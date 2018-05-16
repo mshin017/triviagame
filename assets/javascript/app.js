3f764c498e948a763a2fe60d38f902b8bea53d23
Michelle Shin <mshin017@ucr.edu>
2018-05-16 14:24:47 -0700

    updating homework 2 5/16 2:24

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,35 +1,62 @@
-document.write("<script src='questions.js' type='text/javascript'></script>");
 //PSEUDO CODE
-// Put links to image array 
-var images = [];
 // Variables showImage will hold the setInterval when we start the slideshow
 // var showImage;
 //count will keep track of the index of the current display
 // var count = 0;
-$.getScript("questions.js",function() {});
+var chosenQuestion = questionArray[Math.floor(Math.random() * questionArray.length)];
+var question = chosenQuestion.questions;
+var choices = chosenQuestion.choices;
+var correct = chosenQuestion.correct;
+    console.log(chosenQuestion);
+    console.log(question);
+    console.log(choices);
+    console.log(correct);
 
-$("#start").click(start)
+//-----------------------------
 
 window.onload=function(){
     $(".buttons").hide();
     $("#time").hide();
 }
 
+$("#start").click(start)
 
 function start() {
     $("#start").hide(); $(".buttons").show(); $("#time").show();
     displayQuestion();
-
+    displayChoices();
 }
-function displayQuestion () {
+
+function displayQuestion(){
+    $("#questions").html("<p> Q: " + question + "</p>")
     timer();
-    $("#questions").html("<p> Q: " + questions[0] + "</p>");
-    $("#opt-1").append(choice1[0]);
-    $("#opt-2").append(choice2[0]);
-    $("#opt-3").append(choice3[0]);
-    $("#opt-4").append(choice4[0]);
+}
+function displayChoices(){
+    $("#opt-1").append(choices[0]);
+    $("#opt-2").append(choices[1]);
+    $("#opt-3").append(choices[2]);
+    $("#opt-4").append(choices[3]);
 
 }
+
+
+// function nextQuestionInRound () {
+//     chosenQuestion = questionArray[Math.floor(Math.random() * questionArray.length)];
+//     question = chosenQuestion.questions;
+//     choices = chosenQuestion.choices;
+//     correct = chosenQuestion.correct;
+// }
+// nextQuestionInRound ();
+
+// function displayQuestion () {
+//     timer();
+//     $("#questions").html("<p> Q: " + questions[0] + "</p>");
+//     $("#opt-1").append(choice1[0]);
+//     $("#opt-2").append(choice2[0]);
+//     $("#opt-3").append(choice3[0]);
+//     $("#opt-4").append(choice4[0]);
+
+// }
 
 
 function timer () {  
--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,60 +1,169 @@
-var questions = [
-    "Which Disney movie was the first to release a soundtrack?",
-    "Which easter egg is in every Disney Pixar movie?",
-    "Which psychological disorder was named after a Disney character?",
-    "Lady and the Tramp was inspired by real dog. What was the name of the dog?",
-    "Which among these Disney films have both parents present and alive during the course of the movie?",
-    "In Monsters Inc., what is Boo's real name?",
-    "Which of these princesses are not officially a Disney princess? ",
-    "How many types of animals were featured in Zootopia?",
-    "Which of these movies are NOT going to be a live action movie?",
+// Put links to image array 
+var images = [];
+
+
+
+var questionArray = [
+    {
+        questions: "Which Disney movie was the first to release a soundtrack? ",
+        choices: 
+            [
+                "Snow White and the Seven Dwarves",
+                "Sleeping Beauty", 
+                "Little Mermaid",
+                "Cinderella",
+            ],
+        correct: "Snow White and the Seven Dwarves"
+    },
+    {
+        questions: " Which easter egg is in every Disney Pixar movie? ",
+        choices: 
+            [
+                "Mickey Mouse",
+                "Pizza Truck", 
+                "A113",
+                "Nemo",
+            ],
+        correct: "A113"
+    },
+    {
+        questions: "Which psychological disorder was named after a Disney character?",
+        choices: 
+            [
+                "Nemo",
+                "Bambi", 
+                "Pinocchio",
+                "Simba",
+            ],
+        correct: "Bambi"
+    },
+    {
+        questions: "Lady and the Tramp was inspired by real dog. What was the name of the dog?",
+        choices: 
+            [
+                "Lady",
+                "Trump", 
+                "Walt",
+                "Dan",
+            ],
+        correct: "Lady"
+    },
+    {
+        questions: "Which among these Disney films have both parents present and alive during the course of the movie??",
+        choices: 
+            [
+                "Peter Pan",
+                "Hunchback of Notre Dame", 
+                "Treasure Planet",
+                "Toy Story",
+            ],
+        correct: "Peter Pan"
+    },
+    {
+        questions: "In Monsters Inc., what is Boo's real name?",
+        choices: 
+            [
+                "Daisy",
+                "Charlotte", 
+                "Jane",
+                "Mary",
+            ],
+        correct: "Mary"
+    },
+    {
+        questions: "Which of these princesses are not officially a Disney princess? ",
+        choices: 
+            [
+                "Anna",
+                "Tiana", 
+                "Merida",
+                "Mulan",
+            ],
+        correct: "Anna"
+    },
+    {
+        questions: "How many types of animals were featured in Zootopia?",
+        choices: 
+            [
+                "59",
+                "64", 
+                "83",
+                "96",
+            ],
+        correct: "64"
+
+    },
+    {
+        questions: "Which of these movies are NOT going to be a live action movie? ",
+        choices: 
+            [
+                "Mulan",
+                "Lion King", 
+                "Tangled",
+                "Aladdin",
+            ],
+        correct: "Tangled"
+        
+    },
 ]
 
-var choice1 = [
-    "Snow White and the Seven Dwarves",
-    "Mickey Mouse",
-    "Nemo",
-    "Lady ",
-    "Peter Pan",
-    "Daisy",
-    "Anna",
-    "9",
-    "Mulan",
-]
+// var questions = [
+//     "Which Disney movie was the first to release a soundtrack?",
+//     "Which easter egg is in every Disney Pixar movie?",
+//     "Which psychological disorder was named after a Disney character?",
+//     "Lady and the Tramp was inspired by real dog. What was the name of the dog?",
+//     "Which among these Disney films have both parents present and alive during the course of the movie?",
+//     "In Monsters Inc., what is Boo's real name?",
+//     "Which of these princesses are not officially a Disney princess? ",
+//     "How many types of animals were featured in Zootopia?",
+//     "Which of these movies are NOT going to be a live action movie?",
+// ]
 
-var choice2 = [
-    "Sleeping Beauty",
-    "Pizza Truck",
-    "Bambi",
-    "Tramp",
-    "Hunchback of Notre Dame",
-    "Charlotte",
-    "Tiana",
-    "64",
-    "Lion King",
-]
+// var choice1 = [
+//     "Snow White and the Seven Dwarves",
+//     "Mickey Mouse",
+//     "Nemo",
+//     "Lady ",
+//     "Peter Pan",
+//     "Daisy",
+//     "Anna",
+//     "9",
+//     "Mulan",
+// ]
 
-var choice3 = [
-    "Little Mermaid",
-    "A113",
-    "Pinocchio",
-    "Walt",
-    "Treasure Planet",
-    "Jane",
-    "Merida",
-    "83",
-    "Tangled",
-]
+// var choice2 = [
+//     "Sleeping Beauty",
+//     "Pizza Truck",
+//     "Bambi",
+//     "Tramp",
+//     "Hunchback of Notre Dame",
+//     "Charlotte",
+//     "Tiana",
+//     "64",
+//     "Lion King",
+// ]
 
-var choice4 = [ 
-    "Cinderella",
-    "Nemo",
-    "Simba",
-    "Dan",
-    "Toy Story",
-    "Mary",
-    "Mulan",
-    "96",
-    "Aladdin",
+// var choice3 = [
+//     "Little Mermaid",
+//     "A113",
+//     "Pinocchio",
+//     "Walt",
+//     "Treasure Planet",
+//     "Jane",
+//     "Merida",
+//     "83",
+//     "Tangled",
+// ]
 
-]
+// var choice4 = [ 
+//     "Cinderella",
+//     "Nemo",
+//     "Simba",
+//     "Dan",
+//     "Toy Story",
+//     "Mary",
+//     "Mulan",
+//     "96",
+//     "Aladdin",
+
+// ]
